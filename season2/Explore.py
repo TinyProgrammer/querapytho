b9b3df595b584216127c90cb203a0617fe08b31b
mamad <mollataiefeh@gmail.com>
2020-07-20 02:28:52 +0430

    problam solved

---
+++ season2/Explore.py
@@ -0,0 +1,22 @@
+import os
+def explore(pf,add):
+    pf = '.'+pf
+    tmp=os.walk(add)
+    tmp = list(tmp)
+    ls={}
+
+    print(pf)
+
+    for i in tmp:
+        c=0
+        for j in i[2]:
+            if pf in j:
+                c=c+1
+        if c != 0:
+            ls[i[0]]=c
+        c=0
+    return ls 
+    pass
+
+print(explore('py','.'))
+
